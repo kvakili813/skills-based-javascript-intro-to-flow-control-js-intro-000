158bdedc8859a849882e701a0627a24516aa00cf
kvakili813 <kvakili813@gmail.com>
2017-09-19 00:05:58 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,14 @@
 function basicTeenager(age) {
-
+  age = 14
+  if (age) {>= 13 && age <= 19}
+    return "You are a teenager!"
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19)
+    return "You are a teenager!"
+} else {
+    return "You are not a teenager!"
 }
 
 function ageChecker(age) {
