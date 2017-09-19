9aa705ba81d478479514d09a4f801857196800b2
kvakili813 <kvakili813@gmail.com>
2017-09-19 01:45:48 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -17,7 +17,7 @@ function ageChecker(age) {
     if (age >= 13 && age <= 19) {
       return "You are a teenager!"
 } else if (age < 12) {
-  return "You are a kid"
+  "You are a kid"
 } else {
   "You are a grownup"
 }
